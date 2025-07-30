6112b70d9bcd243ae90eb9e0ce1ba1cf1c59f9d9
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-30 09:35:33 -0300

    Adiciona e configura fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,21 @@
 import type { Metadata } from "next";
+import { Roboto, Fjalla_One } from "next/font/google";
 import "./globals.css";
+
+/* Configura variáveis para as fontes */
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "swap",
+});
+
+const fjallaOne = Fjalla_One({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-fjalla",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "PetShop",
@@ -13,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
+      <body className={`${roboto.variable} ${fjallaOne.variable}`}>
         <header>
           <h1>PetShop</h1>
         </header>
