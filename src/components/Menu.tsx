83ff16ee5b502c3ac2ce8790e4c6981ce4c53f73
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-31 10:49:19 -0300

    Aplica 'use client' e usePathname
    Definir link ativo

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,14 +1,35 @@
+"use client";
+
 // src/components/Menu.tsx
 import Link from "next/link";
 import estilos from "./Menu.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Menu() {
+  const pathname = usePathname();
   return (
     <nav className={estilos.menu}>
-      <Link href="/">Blog</Link>
-      <Link href="/produtos">Produtos</Link>
-      <Link href="/sobre">Sobre</Link>
-      <Link href="/contato">Contato</Link>
+      <Link href="/" className={pathname === "/" ? estilos.ativo : ""}>
+        Blog
+      </Link>
+      <Link
+        href="/produtos"
+        className={pathname === "/produtos" ? estilos.ativo : ""}
+      >
+        Produtos
+      </Link>
+      <Link
+        href="/sobre"
+        className={pathname === "/sobre" ? estilos.ativo : ""}
+      >
+        Sobre
+      </Link>
+      <Link
+        href="/contato"
+        className={pathname === "/contato" ? estilos.ativo : ""}
+      >
+        Contato
+      </Link>
     </nav>
   );
 }
