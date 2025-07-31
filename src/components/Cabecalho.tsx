194648cef2a95d09a87c20201f00b6811342dde6
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-31 08:51:58 -0300

    Aplica módulo CSS do Cabecalho

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,14 +1,17 @@
 // src/components/Cabecalho.tsx
 import Link from "next/link";
 import Menu from "./Menu";
+import estilos from "./Cabecalho.module.css";
 
 export default function Cabecalho() {
   return (
-    <header>
-      <h1>
-        <Link href="/">PetShop</Link>
-      </h1>
-      <Menu />
+    <header className={estilos.topo}>
+      <div className="limitador">
+        <h1>
+          <Link href="/">PetShop</Link>
+        </h1>
+        <Menu />
+      </div>
     </header>
   );
 }
