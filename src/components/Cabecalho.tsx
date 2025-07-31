91c0f30f4248efa025390e3b06bafe342a5b4ced
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-31 09:03:44 -0300

    Aplica componente Image para logo

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -2,13 +2,22 @@
 import Link from "next/link";
 import Menu from "./Menu";
 import estilos from "./Cabecalho.module.css";
+import Image from "next/image";
 
 export default function Cabecalho() {
   return (
     <header className={estilos.topo}>
       <div className="limitador">
         <h1>
-          <Link href="/">PetShop</Link>
+          <Link href="/">
+            <Image
+              src="/images/logo.png"
+              alt="Patinha dentro de um coração"
+              width={48}
+              height={48}
+            />
+            PetShop
+          </Link>
         </h1>
         <Menu />
       </div>
