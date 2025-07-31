3222f08861709be48a1a33db8562207d9f242146
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-31 11:34:29 -0300

    Adiciona SVG à page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 // src/app/not-found.tsx
+import Link from "next/link";
 import estilos from "./not-found.module.css";
 import { Metadata } from "next";
 
@@ -11,6 +12,16 @@ export default function Pagina404() {
   return (
     <section className={estilos.conteudo}>
       <h2>Ops! Página não encontrada!</h2>
+
+      <div className={estilos.imagem404}>
+        <img
+          src="/images/404.svg"
+          alt="Cachorrinho com cara triste e nariz vermelho com o símbolo de interrogação"
+        />
+        <p>
+          <Link href="/">Voltar para a página inicial</Link>
+        </p>
+      </div>
     </section>
   );
 }
