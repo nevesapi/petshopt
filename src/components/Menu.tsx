d3f33e30252ab6f2ea5c560e8664f6764559cef6
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-31 10:29:17 -0300

    Adiciona estilos do menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,10 @@
 // src/components/Menu.tsx
 import Link from "next/link";
+import estilos from "./Menu.module.css";
 
 export default function Menu() {
   return (
-    <nav>
+    <nav className={estilos.menu}>
       <Link href="/">Blog</Link>
       <Link href="/produtos">Produtos</Link>
       <Link href="/sobre">Sobre</Link>
