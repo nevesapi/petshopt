f04bfef20b58a36944192490ee33a43176dd4f51
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-08-04 09:27:08 -0300

    Remove comentários

--- src/components/ListaPosts.tsx
+++ src/components/ListaPosts.tsx
@@ -2,9 +2,6 @@
 import { Post } from "@/types/Post";
 import estilos from "./ListaPosts.module.css";
 
-/* Configurando um tipo para este componente e,
-neste tipo, definimos a prop posts relacionando ela ao tipo
-Post criado anteriormente. */
 type ListaPostsProps = {
   posts: Post[];
 };
