9c722b8c1bd848059aab9555641d52f2e3129e04
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-08-04 09:15:37 -0300

    Ajusta cache para o next e tipa Posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,19 @@
 import ListaPosts from "@/components/ListaPosts";
 import estilos from "./page.module.css";
 import arrayPosts from "@/data/array-posts";
+import { Post } from "@/types/Post";
 
 export default async function Home() {
-  const resposta = await fetch(`http://localhost:2112/posts`);
+  const resposta = await fetch(`http://localhost:2112/posts`, {
+    // Revalidamos o cache do next a cada requisição para garantir que os dados estejam sempre atualizados
+    next: { revalidate: 0 },
+  });
 
   if (!resposta.ok) {
     throw new Error("Erro ao buscar os posts: " + resposta.statusText);
   }
 
-  const posts = await resposta.json();
+  const posts: Post[] = await resposta.json();
   console.log(posts);
 
   return (
