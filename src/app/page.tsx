24cda886c019b95f44b9878ee9eb23547452e6e1
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-08-04 09:06:20 -0300

    Aplica fetch com asyn/await na Home
    Acesso ao endpoint de posts na fake-api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,16 @@ import ListaPosts from "@/components/ListaPosts";
 import estilos from "./page.module.css";
 import arrayPosts from "@/data/array-posts";
 
-export default function Home() {
+export default async function Home() {
+  const resposta = await fetch(`http://localhost:2112/posts`);
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar os posts: " + resposta.statusText);
+  }
+
+  const posts = await resposta.json();
+  console.log(posts);
+
   return (
     <section className={estilos.conteudo}>
       <h2>Pet Notícias</h2>
